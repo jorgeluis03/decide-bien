6d180c58dafbca5f4da18fbf6837e03d69d2fba1
xt8736 <xt8736@rimac.com.pe>
2025-03-08 21:17:09 -0500

    feat: proxy local para obtener imagen desde la url

--- components/screens/PartidoItem.tsx
+++ components/screens/PartidoItem.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Image, StyleSheet, TouchableOpacity, Linking } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
+import axios from 'axios';
 
 interface Personero {
     tipo: string;
@@ -23,12 +24,16 @@ interface Props {
 }
 
 export default function PartidoItem({ partido }: Props) {
+    const proxyUrl = "http://192.168.18.24:8080/api/v1/proxy?url=";
+    const targetUrl = encodeURIComponent("https://www.congreso.gob.pe/Storage/tbl_congresistas/fld_47_Fotografia_file/1160-n5Ta8Yr8Pg9Vj5N.jpg");
+
     return (
         <View style={styles.itemContainer}>
             <Image
-                source={{ uri: "https://images.unsplash.com/photo-1735558427078-43aa416a3145?w=600&auto=format&fit=crop&q=60" }}
+                source={{ uri: proxyUrl + targetUrl }}
                 style={styles.image}
             />
+
             <View style={styles.textContainer}>
                 <ThemedText style={styles.title}>{partido.nombre}</ThemedText>
                 <ThemedText style={styles.info}>Inscripción: {partido.fecha_inscripcion}</ThemedText>
